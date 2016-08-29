8fb53a3980936f5118d6b4b63f3005c6a8037bd8
richardyam1 <richardyam1@yahoo.com>
2016-08-29 16:57:46 -0400

    Reduce the size of solar panel and start click events for buttons

---
+++ Javascript_Calculator/Javascript Calculator.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+	var input = "";
+	var equation = "";
+	$("#input").html("0");
+	$("#equation").html("0");
+
+	$("clear_all").on("click", function(){
+
+	});
+
+	$("clear_one").on("click", function(){
+
+	});
+
+	$("nine").on("click", function(){
+		input += "9";	
+			$("#input").html(input);
+
+	});
+
+	$("eight").on("click", function(){
+
+	});
+
+	$("seven").on("click", function(){
+
+	});
+
+	$("six").on("click", function(){
+
+	});
+
+	$("five").on("click", function(){
+
+	});
+
+	$("four").on("click", function(){
+
+	});
+
+	$("three").on("click", function(){
+
+	});
+
+	$("two").on("click", function(){
+
+	});
+
+	$("one").on("click", function(){
+
+	});
+
+	$("zero").on("click", function(){
+
+	});
+
+	$("decimal").on("click", function(){
+
+	});
+
+	$("plus").on("click", function(){
+
+	});
+
+	$("minus").on("click", function(){
+
+	});
+
+	$("multiply").on("click", function(){
+
+	});
+
+	$("divide").on("click", function(){
+
+	});
+
+	$("equal").on("click", function(){
+
+	});
+
+});
