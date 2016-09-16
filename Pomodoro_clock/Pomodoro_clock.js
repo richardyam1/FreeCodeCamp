b0e9536f40c111d71c57f3ede28c9fe1b879a6d5
richardyam1 <richardyam1@yahoo.com>
2016-09-16 17:02:20 -0400

    Pause countdown when ".timer" button is pressed.

--- Pomodoro_clock/Pomodoro_clock.js
+++ Pomodoro_clock/Pomodoro_clock.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   var break_minute = 5;
   var work_minute = 25;
+  //Set to 5 for testing purposes
+  var seconds = 0;
   var paused = true;
   $("#break_minute").text(break_minute);
   $("#work_minute").text(work_minute);
@@ -39,20 +41,38 @@ $(document).ready(function(){
     });
 
     $(".timer").on("click", function(){
-        var seconds = 5;
-  
-        setInterval(function() {
-        if(work_minute > -1 && seconds === -1){
-          seconds = 5;
-          work_minute--;
-        }
-        if(seconds < 10){
-          seconds = '0' + seconds;
-        }
-        $(".timer").html("Session" + "<br>" + "<br>" + (work_minute )  + ":" + seconds);
-        seconds--;
+       if(paused === true){
+        paused = false;
+       }
+
+       else if(paused === false){
+        paused = true;
+       }
+      
+        var c = setInterval(function() {
+          if(work_minute > 0 && seconds === -1){
+            //Set to 5 for testing purposes
+            seconds = 5;
+            work_minute--;
+          }
+          if(seconds < 10){
+            seconds = '0' + seconds;
+          }
+          $(".timer").html("Pause Session" + "<br>" + "<br>" + (work_minute )  + ":" + seconds);
+          seconds--;
+          if(paused === true){
+            $(".timer").html("Resume Session" + "<br>" + "<br>" + (work_minute )  + ":" + seconds);
+
+            clearInterval(c);
+          }
 
         }, 1000);
+        
+       
+
+       
+
+        
     });
     
   });
