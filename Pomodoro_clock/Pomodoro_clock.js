4d38e512247cfc0704739edb0739c10e3ea12ebe
richardyam1 <richardyam1@yahoo.com>
2016-09-19 16:58:30 -0400

    Combine both work and break countdown into one countdown.  Transition from work to break when time runs out

--- Pomodoro_clock/Pomodoro_clock.js
+++ Pomodoro_clock/Pomodoro_clock.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   //Set to 5 for testing purposes
   var seconds = 0;
   var paused = true;
+  var break_time = false;
   $("#break_minute").text(break_minute);
   $("#work_minute").text(work_minute);
   $(".timer").html("Start Session" + "<br>" + "<br>" + work_minute);
@@ -48,8 +49,35 @@ $(document).ready(function(){
        else if(paused === false){
         paused = true;
        }
-      
-        var c = setInterval(function() {
+       /*
+       if(break_time === true){
+         var break_countdown = setInterval(function(){
+            if(break_minute > 0 && seconds === -1){
+              seconds = 5;
+              break_minute--;
+            }
+            if(seconds < 10){
+              seconds = '0' + seconds;
+            }
+
+            else if(break_minute === 0 && seconds === 0){
+              break_time = false;
+              work_countdown();
+            }
+
+            $(".timer").html("Pause Session" + "<br>" + "<br>" + (break_minute )  + ":" + seconds);
+            seconds--;
+            
+            if(paused === true){
+              $(".timer").html("Resume Session" + "<br>" + "<br>" + (break_minute)  + ":" + seconds);
+
+              clearInterval(break_countdown);
+            }
+          },1000);
+       }
+       });
+      if(break_time === false){
+        var work_countdown = setInterval(function() {
           if(work_minute > 0 && seconds === -1){
             //Set to 5 for testing purposes
             seconds = 5;
@@ -58,22 +86,74 @@ $(document).ready(function(){
           if(seconds < 10){
             seconds = '0' + seconds;
           }
+
+          else if(work_minute === 0 && seconds === 0){
+            break_time = true;
+            break_countdown();
+          }
           $(".timer").html("Pause Session" + "<br>" + "<br>" + (work_minute )  + ":" + seconds);
           seconds--;
+          
           if(paused === true){
             $(".timer").html("Resume Session" + "<br>" + "<br>" + (work_minute )  + ":" + seconds);
 
-            clearInterval(c);
+            clearInterval(work_countdown);
           }
 
         }, 1000);
-        
-       
+      }*/
 
-       
+      var count_down = setInterval(function(){
+        if(break_time === false){
+          if(work_minute === 0 && seconds === 0){
+            break_time = true;
+          }
 
+          else if(work_minute > 0 && seconds === -1){
+            //Set to 5 for testing purposes
+            seconds = 5;
+            work_minute--;
+          }
+          if(seconds < 10){
+            seconds = '0' + seconds;
+          }
+
+          $(".timer").html("Pause Session" + "<br>" + "<br>" + (work_minute )  + ":" + seconds);
+          seconds--;
+          
+          if(paused === true){
+            $(".timer").html("Resume Session" + "<br>" + "<br>" + (work_minute )  + ":" + seconds);
+
+            clearInterval(work_countdown);
+          }
+        else if(break_time === true){
+          if(break_minute === 0 && seconds === 0){
+              break_time = false;
+            }
+
+          else if(break_minute > 0 && seconds === -1){
+              seconds = 5;
+              break_minute--;
+            }
+            if(seconds < 10){
+              seconds = '0' + seconds;
+            }
+
+            
+            $(".timer").html("Pause Break" + "<br>" + "<br>" + (break_minute )  + ":" + seconds);
+            seconds--;
+            
+            if(paused === true){
+              $(".timer").html("Resume Break" + "<br>" + "<br>" + (break_minute)  + ":" + seconds);
+
+              clearInterval(break_countdown);
+            }
+        }
+        }
+      },1000);
         
     });
     
-  });
   
+  
+});
