4bfc6f351295d3f1f1f90430a016adc4ae39b4ee
richardyam1 <richardyam1@yahoo.com>
2016-09-25 17:12:54 -0400

    Resolve issue with pausing timer and reset values when "-" or "+" is pressed

--- Pomodoro_clock/Pomodoro_clock.js
+++ Pomodoro_clock/Pomodoro_clock.js
@@ -1,120 +1,124 @@
 $(document).ready(function(){
-  var break_minute = 5;
-  var work_minute = 1;
+  var initial_break = 5;
+  var initial_work = 1;
  
   //Set to 5 for testing purposes
   var seconds = 5;
   var paused = true;
   var rest_time = false;
-  $("#break_minute").text(break_minute);
-  $("#work_minute").text(work_minute);
-  $(".timer").html("Start Session" + "<br>" + "<br>" + work_minute);
+  var countdown;
+  $("#initial_break").text(initial_break);
+  $("#initial_work").text(initial_work);
+  $(".timer").html("Start Session" + "<br>" + "<br>" + initial_work);
 
   $("#minus_break").on("click",function(){
-    if(break_minute > 1){
-      break_minute--;
-      $("#break_minute").text(break_minute);
+    if(initial_break > 1){
+      clearInterval(countdown);
+      seconds = 5;
+      paused = true;
+      initial_break--;
+      $("#initial_break").text(initial_break);
+
     }
   });
    
    $("#plus_break").on("click", function(){
-     if(break_minute < 15){
-       break_minute++;
-       $("#break_minute").text(break_minute);
+     if(initial_break < 15){
+      clearInterval(countdown);
+      seconds = 5;
+      paused = true;
+
+       initial_break++;
+       $("#initial_break").text(initial_break);
      }
    });
     
     $("#minus_work").on("click", function(){
-      if(work_minute > 1){
-        work_minute--;
-        $("#work_minute").text(work_minute);
-        $(".timer").html("Start Session" + "<br>" + "<br>" + work_minute);
+      if(initial_work > 1){
+        clearInterval(countdown);
+        seconds = 5;
+        paused = true;
+
+        initial_work--;
+        $("#initial_work").text(initial_work);
+        $(".timer").html("Start Session" + "<br>" + "<br>" + initial_work);
 
       }
     });
     
     $("#plus_work").on("click", function(){
-      if(work_minute < 40){
-        work_minute++;
-        $("#work_minute").text(work_minute);
-        $(".timer").html("Start Session" + "<br>" + "<br>" + work_minute);
+      if(initial_work < 40){
+        clearInterval(countdown);
+        seconds = 5;
+        paused = true;
+        initial_work++;
+        $("#initial_work").text(initial_work);
+        $(".timer").html("Start Session" + "<br>" + "<br>" + initial_work);
 
       }
     });
 
     $(".timer").on("click", function(){
-       if(paused === true){
-        paused = false;
-       }
-
-       else if(paused === false){
-        paused = true;
-       }
+       
+    if(paused === true){
        //Stores initial minutes to reset to during transition
-       var break_down = break_minute;
-       var work_down = work_minute;
 
-      var count_down = setInterval(function(){
-        if(rest_time === false && paused === false){
-          
+       var break_minute = initial_break;
+       var work_minute= initial_work;
+      countdown = setInterval(function(){
+       
+          if(rest_time === false){
             if(seconds < 10){
               seconds = '0' + seconds;
             }
-            $(".timer").html("Pause Session" + "<br>" + "<br>" + (work_down )  + ":" + seconds);
+            $(".timer").html("Pause Session" + "<br>" + "<br>" + (work_minute)  + ":" + seconds);
             seconds--;
-            
-         
-
-          if(work_down === 0 && seconds === -1){
+            if(work_minute === 0 && seconds === -1){
             rest_time = true;
-            work_down = work_minute;
+            work_minute= initial_work;
             seconds = 5;
           }
 
-          else if(work_down > -1 && seconds === -1){
+           else if(work_minute > -1 && seconds === -1){
             //Set to 5 for testing purposes
-            seconds = 5;
-            work_down--;
+             seconds = 5;
+
+            work_minute--;
+
           }
-          
 
          
         }
-        else if(rest_time === true && paused === false){
+        else if(rest_time === true){
             if(seconds < 10){
               seconds = '0' + seconds;
             }
  
-            $(".timer").html("Pause Break" + "<br>" + "<br>" + (break_down)  + ":" + seconds);
+            $(".timer").html("Pause Break" + "<br>" + "<br>" + (break_minute)  + ":" + seconds);
             seconds--;
             
-            if(paused === true){
-              $(".timer").html("Resume Break" + "<br>" + "<br>" + (break_down)  + ":" + seconds);
-
-              clearInterval(countdown);
-            }
-          if(break_down === 0 && seconds === -1){
+            
+          if(break_minute === 0 && seconds === -1){
               rest_time = false;
-              break_down = break_minute;
+              break_minute = initial_break;
               seconds = 5;
             }
 
-          else if(break_down > -1 && seconds === -1){
+          else if(break_minute > -1 && seconds === -1){
               seconds = 5;
-              break_down--;
+              break_minute--;
             }
             
         }
         
       },1000);
-        if(paused === true){
-            if(seconds < 10){
-              seconds = '0' + seconds;
+}
+        else{
+           clearInterval(countdown);
+    
           }
-            $(".timer").html("Resume Session" + "<br>" + "<br>" + (work_down )  + ":" + seconds);
-            seconds = seconds;
-            clearInterval(countdown);
-          }
+          paused = !paused;
+
     });
     
   
