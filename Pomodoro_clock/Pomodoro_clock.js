52969e385fcebc34c3ae2ae4b91701dbcebc72e8
richardyam1 <richardyam1@yahoo.com>
2016-09-28 16:27:47 -0400

    Change background color for work session.  Add comments as notes.  Remove unneeded code.  Change seconds back to correct value.  Tidy up code

--- Pomodoro_clock/Pomodoro_clock.js
+++ Pomodoro_clock/Pomodoro_clock.js
@@ -1,57 +1,47 @@
 $(document).ready(function(){
   var initial_break = 5;
-  var initial_work = 1;
-  var break_minute;
-  var work_minute;
-  //Set to 5 for testing purposes
-  var seconds = 5;
-  var paused=  true;
+  var initial_work = 25;
+  //Store minutes in seperate value so timer can reset to original value when timer fully elapsed
+  var break_minute = 5;
+  var work_minute = 25;
+  var seconds = 0;
+  //Stops the timer
+  var paused=  false;
+  //Switch back and forth from break time
   var rest_time = false;
+  //Check session status so user can't modify time mid-session.
+  var session = false;
   var audio = new Audio("http://www.mediacollege.com/downloads/sound-effects/city/factorywhistle-01.mp3");
+  //Will store setInterval function
   var countdown;
+
   $("#initial_break").text(initial_break);
   $("#initial_work").text(initial_work);
   $("#session").text("Start Session");
   $("#minute").text(initial_work);
 
   $("#minus_break").on("click",function(){
-    if(initial_break > 1){
-      clearInterval(countdown);
-      seconds = 5;
-      paused = true;
+    if(initial_break > 1 && session === false){
       initial_break--;
       break_minute = initial_break;
-      $("#session").text("Resume");
       $("#initial_break").text(initial_break);
-      
     }
   });
    
    $("#plus_break").on("click", function(){
-     if(initial_break < 15){
-      clearInterval(countdown);
-      seconds = 5;
-      paused = true;
+     if(initial_break < 15 && session === false){
       initial_break++;
       break_minute = initial_break;
-      $("#session").text("Resume");
-
        $("#initial_break").text(initial_break);
 
      }
    });
     
     $("#minus_work").on("click", function(){
-      if(initial_work > 1){
-        clearInterval(countdown);
-        seconds = 5;
-        paused = true;
-
+      if(initial_work > 1 && session === false){
         initial_work--;
         work_minute = initial_work;
         $("#minute").text(initial_work);
-        $("#session").text("Resume");
-
         $("#initial_work").text(initial_work);
 
 
@@ -59,100 +49,94 @@ $(document).ready(function(){
     });
     
     $("#plus_work").on("click", function(){
-      if(initial_work < 40){
-        clearInterval(countdown);
-        seconds = 5;
-        paused = true;
+      if(initial_work < 40 && session === false){
         initial_work++;
         work_minute = initial_work;
         $("#minute").text(initial_work);
         $("#initial_work").text(initial_work);
-        $("#session").text("Resume");
-
-
       }
     });
-     break_minute = initial_break;
-     work_minute = initial_work;
-    $(".timer").on("click", function(){
- 
-    if(paused === true){
-       //Stores initial minutes to reset to during transition
-       
-       
-      countdown = setInterval(function(){
-       
-          if(rest_time === false){
-            if(seconds < 10){
-              seconds = '0' + seconds;
-            }
-            $("#session").text("Pause Work");
-            $("#minute").text(work_minute + ":" + seconds);
-            seconds--;
-
-            if(work_minute === 0 && seconds === -1){
-            rest_time = true;
-            work_minute= initial_work;
-            seconds = 5;
-            $("html body").animate({ backgroundColor: "#03FBCA" });
-            audio.play();
-          }
-
-           else if(work_minute > -1 && seconds === -1){
-            //Set to 5 for testing purposes
-             seconds = 5;
-
-            work_minute--;
-
-          }
-
-         
-        }
-        else if(rest_time === true){
-            if(seconds < 10){
-              seconds = '0' + seconds;
-            }
-            $("#session").text("Pause Break");
-            $("#minute").text(break_minute + ":" + seconds);
-            seconds--;
-            
-            
-          if(break_minute === 0 && seconds === -1){
-              rest_time = false;
-              break_minute = initial_break;
-              seconds = 5;
-              $("html body").animate({ backgroundColor: "#A3D826" });
-              audio.play();
-
-            }
-
-          else if(break_minute > -1 && seconds === -1){
-              seconds = 5;
-              break_minute--;
-            }
-            
-        }
-        
-      },1000);
-}
-        else{
-          $("#session").text("Resume");
-           clearInterval(countdown);
     
+    $(".timer").on("click", function(){
+      session = true;
+      //If session is not paused, proceed with countdown
+      if(paused === false){
+        countdown = setInterval(function(){
+            //Session time
+            if(rest_time === false){
+              if(seconds < 10){
+                seconds = '0' + seconds;
+              }
+              $("#session").text("Pause Session");
+              $("#minute").text(work_minute + ":" + seconds);
+              seconds--;
+
+              if(work_minute === 0 && seconds === -1){
+                rest_time = true;
+                work_minute= initial_work;
+                seconds = 0;
+                $("html body").animate({ backgroundColor: "#03FBCA" });
+                audio.play();
+            }
+
+             else if(work_minute > -1 && seconds === -1){
+               seconds = 59;
+              work_minute--;
+
+            }
+
+           
           }
-          paused = !paused;
+          //Break time
+          else if(rest_time === true){
+              if(seconds < 10){
+                seconds = '0' + seconds;
+              }
+              $("#session").text("Pause Break");
+              $("#minute").text(break_minute + ":" + seconds);
+              seconds--;
+              
+              
+            if(break_minute === 0 && seconds === -1){
+                rest_time = false;
+                break_minute = initial_break;
+                seconds = 0;
+                $("html body").animate({ backgroundColor: "#74D925" });
+                audio.play();
 
-    });
+              }
 
+            else if(break_minute > -1 && seconds === -1){
+                seconds = 59;
+                break_minute--;
+              }
+              
+          }
+          
+        },1000);
+  }
+          //When user pauses session
+          else{
+            $("#session").text("Resume");
+             clearInterval(countdown);
+      
+            }
+            //Paused value changes with each click
+            paused = !paused;
+
+      });
+  
     $(".reset").on("click", function(){
         clearInterval(countdown);
         $("#initial_break").text(initial_break);
         $("#initial_work").text(initial_work);
         $("#session").text("Start Session");
         $("#minute").text(initial_work);
-        seconds = 5;
-        paused=  true;
+        $("html body").animate({ backgroundColor: "#74D925" });
+        seconds = 0;
+        paused=  false;
         rest_time = false;
+        session = false;
     });
     
   
