1ecedc41fc1a3f0a339e3a756a1753f4650ecede
richardyam1 <richardyam1@yahoo.com>
2016-10-05 16:57:21 -0400

    Put in "X" or "O" if panel is clicked on and is empty.

--- Tictactoe/Tictactoe.js
+++ Tictactoe/Tictactoe.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+	var turn1 = true;
+	$(".panel").click(function(){
+		var mark = "#" + document.getElementById(this.id).id;
+		if(document.getElementById(this.id).innerHTML === ""){
+			if(turn1){
+				$(mark).text("X");
 
+			}
+			else{
+				$(mark).text("O");
+			}
+			turn1 = !turn1;
+		}
+	});
 });
