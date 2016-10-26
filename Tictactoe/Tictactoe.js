2ba52b8159cf338d3d4ab20248c3d459f97a3e6b
richardyam1 <richardyam1@yahoo.com>
2016-10-26 17:09:41 -0400

    Add condition when game is a draw

--- Tictactoe/Tictactoe.js
+++ Tictactoe/Tictactoe.js
@@ -6,9 +6,16 @@ $(document).ready(function(){
 	var player1;
 	var player2;
 	var winner = [
-		[1,2,3],
-		[4,5,6]
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 8, 9],
+		[1, 4, 7],
+		[2, 5, 8],
+		[3, 6, 9],
+		[1, 5, 9],
+		[3, 5, 7]
 	];
+	var plays = 0;
 	
 	var captureSquare = new Audio("http://www.qwizx.com/gssfx/usa/hs-ding.wav");
 
@@ -41,10 +48,12 @@ $(document).ready(function(){
 				if(player1 === "X"){
 					$(this).addClass("x-play");
 					$(this).data("mark", "X");
+					
 				}
 				else if(player1 === "O"){
 					$(this).addClass("o-play");
 					$(this).data("mark", "O");
+
 				}
 				$("#turn1").text("");
 				$("#turn2").text("Player 2 turn");
@@ -65,8 +74,9 @@ $(document).ready(function(){
 
 			}
 			captureSquare.play();
-			turn1 = !turn1;
 			checkGame();
+			turn1 = !turn1;
+			plays++;
 		}
 
 	});
@@ -78,6 +88,7 @@ $(document).ready(function(){
 
 		}
 		else{
+			
 			return false;
 		}
 		
@@ -88,11 +99,33 @@ $(document).ready(function(){
 		for(var i = 0; i < winner.length; i++){
 			var win = gameOver(winner[i][0], winner[i][1], winner[i][2]);
 			if (win){
+				if (turn1){
+					score1++;
+					$("#score1").text(score1);
+					$("#turn1").text("");
+					$("#turn2").text("Player 2 turn");
+
+				}
+				else{
+					score2++;
+					$("#score2").text(score2);
+					$("#turn1").text("Player 1 turn");
+					$("#turn2").text("");
+				}
 				alert("test");
 				resetBoard();
-			}
-		}
 
+				}
+				
+			else if(!win){
+				if (plays === 8){
+					alert("draw");
+					resetBoard();
+				}
+				
+			}
+			
+		}
 
 	}
 
@@ -100,10 +133,8 @@ $(document).ready(function(){
 		$(".panel").removeClass('x-play');
 		$(".panel").removeClass('o-play');
 		$(".panel").data("filled", "");
-		turn1 = true;
-		$("#turn1").text("Player 1 turn");
-		$("#turn2").text("");
-
+		plays = 0;
+		
 	}
 	$("#clearBoard").click(function(){
 		resetBoard();
