8a3d5530189dbab44f2cbd3d459f94a4ca6192b5
richardyam1 <richardyam1@yahoo.com>
2016-11-28 16:52:13 -0500

    Make buttons play sound

--- Simon/Simon.js
+++ Simon/Simon.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
 
+	var greenSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+	var redSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+	var yellowSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+	var blueSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+	$("#green").click(function(){
+		greenSound.play();
+	});
+
+	$("#red").click(function(){
+		redSound.play();
+	});
+
+	$("#yellow").click(function(){
+		yellowSound.play();
+	});
+
+	$("#blue").click(function(){
+		blueSound.play();
+	});
 });
