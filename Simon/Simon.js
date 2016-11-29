07ee534111a41c1c0be265c5cb51db49a9a4757d
richardyam1 <richardyam1@yahoo.com>
2016-11-29 17:01:15 -0500

    Change behavior of power switch and buttons when pressed

--- Simon/Simon.js
+++ Simon/Simon.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-
+	var on = false;
+	var start = false;
+	var strict = false
 	var greenSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
 	var redSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
 	var yellowSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
 	var blueSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+
 	$("#green").click(function(){
 		greenSound.play();
+	
 	});
 
 	$("#red").click(function(){
@@ -18,5 +22,26 @@ $(document).ready(function(){
 
 	$("#blue").click(function(){
 		blueSound.play();
+	});
+
+	$("#powerSwitch").click(function(){
+		if(on === false){
+			document.getElementById("switch").style.marginLeft = "50px";
+			on = true;
+		}
+		else if(on === true){
+			document.getElementById("switch").style.marginLeft = "5px";
+			on = false;
+		}
+	});
+
+	$("#start").click(function(){
+		if(start === false){
+			document.getElementById("start").style.backgroundColor = "green";
+			start = true;
+		}
+		else if(start === true){
+			document.getElementById("start").style.backgroundColor = "red";
+		}
 	});
 });
