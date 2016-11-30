bb4aa21804afb9b7b2c4f99b6f983acbf1715a18
richardyam1 <richardyam1@yahoo.com>
2016-11-30 16:57:25 -0500

    Make pads light up when pressed.  Make buttons only work if switch is on.

--- Simon/Simon.js
+++ Simon/Simon.js
@@ -1,28 +1,44 @@
 $(document).ready(function(){
 	var on = false;
 	var start = false;
-	var strict = false
+	var strict = false;
 	var greenSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
 	var redSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
 	var yellowSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
 	var blueSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
-
-	$("#green").click(function(){
-		greenSound.play();
+	var round = 1;
 	
-	});
+		$("#green").click(function(){
+			greenSound.play();
+			document.getElementById("green").style.backgroundColor = "#2BFF4D";
+			lightUp("green");
+		});
 
-	$("#red").click(function(){
-		redSound.play();
-	});
+		$("#red").click(function(){
+			redSound.play();
+			document.getElementById("red").style.backgroundColor = "#7F0000";
+			lightUp("red");
+		});
 
-	$("#yellow").click(function(){
-		yellowSound.play();
-	});
+		$("#yellow").click(function(){
+			yellowSound.play();
+			document.getElementById("yellow").style.backgroundColor= "#E9E080";
+			lightUp("yellow");
 
-	$("#blue").click(function(){
-		blueSound.play();
-	});
+		});
+
+		$("#blue").click(function(){
+			blueSound.play();
+			document.getElementById("blue").style.backgroundColor = "#5D72FF";
+			lightUp("blue");
+		});
+
+
+	function lightUp(color){
+		setTimeout(function(){
+			document.getElementById(color).style.backgroundColor = color;
+		},400);
+	}
 
 	$("#powerSwitch").click(function(){
 		if(on === false){
@@ -31,17 +47,35 @@ $(document).ready(function(){
 		}
 		else if(on === true){
 			document.getElementById("switch").style.marginLeft = "5px";
+			document.getElementById("start").style.backgroundColor = "red";
+			document.getElementById("strict").style.backgroundColor = "yellow";
+			start = false;
+			strict = false;
 			on = false;
+			$("#roundNumber").text("--");
 		}
 	});
 
 	$("#start").click(function(){
-		if(start === false){
+		if(start === false && on === true){
 			document.getElementById("start").style.backgroundColor = "green";
 			start = true;
+			$("#roundNumber").text(round);
 		}
-		else if(start === true){
+		else if(start === true && on === true){
 			document.getElementById("start").style.backgroundColor = "red";
+			start = false;
+		}
+	});
+
+	$("#strict").click(function(){
+		if(strict === false && on === true){
+			document.getElementById("strict").style.backgroundColor = "green";
+			strict = true;
+		}
+		else if(strict === true && on === true){
+			document.getElementById("strict").style.backgroundColor = "yellow";
+			strict = false;
 		}
 	});
 });
